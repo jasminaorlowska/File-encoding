ccb7047066af5d53f979eba79b468b8c95ead119
Jan Witkowski <jw450250@students.mimuw.edu.pl>
2023-06-13 17:10:38 +0200

    uzupelnienie szyfrow

--- CipherCaesar.java
+++ CipherCaesar.java
@@ -1,3 +1,23 @@
 /**Zawiera kodowanie na podstawie szyfru cezara**/
 public class CipherCaesar extends Cipher{
+private final int przesuniecie;
+    public CipherCaesar() {
+        this.przesuniecie=4;
+    }
+    @Override
+    public String encode(String s){
+        StringBuilder outp= new StringBuilder();
+    for(int i=0; i<s.length(); i++){
+        int a=(int)s.charAt(i);
+        if(Character.isLetter(s.charAt(i))){
+            a+=this.przesuniecie;
+            if((Character.isLowerCase(s.charAt(i))&&a>122)||(Character.isUpperCase(s.charAt(i))&&a>90)){
+                a-=26;
+            }
+        }
+        outp.append((char)a);
+    }
+
+        return outp.toString();
+    }
 }
--- CipherMorse.java
+++ CipherMorse.java
@@ -1,3 +1,27 @@
 /**Zawiera kodowanie na podstawie kodu Morse'a*/
 public class CipherMorse extends Cipher{
+    private final char[] literki;
+    private final String[] mors;
+    public CipherMorse() {
+        literki= new char[]{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'w', 'v', 'x', 'y', 'z'};
+        mors= new String[]{".- ", "-... ", "-.-. ", "-.. ",  ". ", "..-. ", "--. ",  ".... ", ".. ", ".--- ", "-.- ",  ".-.. ", "-- ",   "-. ",   "--- ", ".--. ", "--.- ", ".-. ", "... ", "- ", "..- ", "...- ", ".-- ", "-..- ", "-.-- ", "--.. "};
+    }
+    @Override
+    public String encode(String s){
+        StringBuilder outp= new StringBuilder();
+
+        for(int i=0; i<s.length(); i++){
+            boolean a=true;
+            for(int j=0; j<this.literki.length && a;j++){
+                if(s.charAt(i)==this.literki[j]||(int)s.charAt(i)==(int)this.literki[j]-32){
+                    outp.append(this.mors[j]);
+                    a=false;
+                }
+            }//sprawdza czy dany znak w stringu byl jedna z podstawowych liter
+	     //i jesli nie byl nie szyfruje znaku
+            if(a){outp.append(s.charAt(i));}
+        }
+        return outp.toString();
+    }
 }
+
