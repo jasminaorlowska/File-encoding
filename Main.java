ef93848369e7102c4d02e758ffd0715f3aeef507
Jan Witkowski <jw450250@students.mimuw.edu.pl>
2023-06-13 18:11:57 +0200

    poprawka zmienionego przez przypadek przy testowaniu starego maina

--- Main.java
+++ Main.java
@@ -1,15 +1,24 @@
+import java.util.HashMap;
+import java.util.Set;
 
 public class Main {
+    public static void main(String[] args) {
+        int n = 10;
+        CipherCaesar caesar = new CipherCaesar();
+        CipherMorse morse = new CipherMorse();
 
-	public static void main(String[] args){
+        MyText textRead = new MyText();
+        textRead.readMyFile("");
+        Set<String> s = textRead.getWords(n);
 
-Cipher morse=new CipherMorse();
-Cipher cezar=new CipherCaesar();
-String s1="aA sS dddd ,.903 xyz";
-String s2="aA+dfghjkl 90 xyz";
-String s3=morse.encode(s1);
-String s4=cezar.encode(s2);
-System.out.println(s1+s3+s2+s4);
+        for (String word : s) {
+            System.out.println(word);
+        }
+        HashMap<String, String> encoded = textRead.getEncodedWords(caesar);
 
-	}
+        String[] array = {"jeden", "dwa", "trzy"};
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i] + "=" + textRead.getEncoding(array[i]));
+        }
+    }
 }
