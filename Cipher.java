5bbee5e4f25b7c501b88db770be0d6dfaabb54e9
Jan Witkowski <jw450250@students.mimuw.edu.pl>
2023-06-13 17:00:16 +0200

    update Cipher.java

--- Cipher.java
+++ Cipher.java
@@ -1,4 +1,4 @@
 /**Dostarcza metodę kodowania słowa, która odpowiada zakodowanym słowem**/
 public abstract class Cipher {
-    public String encode(String s) {return null;}
+    public abstract String encode(String s);
 }
